aff0018d901fa13c5a8c887fdd68eb060f6c46e6
David Gonzalez <malmstein@gmail.com>
2014-10-17 22:55:33 +0100

    initial commit

---
+++ tv/src/main/java/com/malmstein/androidtvexplorer/leanback/ExplorerActivity.java
@@ -0,0 +1,13 @@
+package com.malmstein.androidtvexplorer.leanback;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExplorerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explorer);
+    }
+}
